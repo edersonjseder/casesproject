c7b420e5b2fe6aae9375b25657c5de139cb2e50b
Ederson de Jesus <edersonjseder@gmail.com>
2022-03-05 11:29:08 +0100

    First commit

---
+++ src/main/java/com/casesproject/casesproject/CasesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.casesproject.casesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasesprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casesproject/casesproject/CasesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casesproject.casesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
